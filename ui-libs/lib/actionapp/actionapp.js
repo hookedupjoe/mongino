e4b26a9fa2ba819753ea7fcd590df2544b95b771
Joseph Francis <hookedupjoe@users.noreply.github.com>
2023-08-21 12:14:22 -0400

    catalog location updated to work with electron

--- ui-libs/lib/actionapp/actionapp.js
+++ ui-libs/lib/actionapp/actionapp.js
@@ -22,12 +22,14 @@ var ActionAppCore = {
     //-    ** How to deploy / if we should deploy catalog resources ?
     dir: {
         catalogs: {
-            common: '/dir/catalogs/_designer/',
-            _designer: '/dir/catalogs/_designer/',
-            _data: '/dir/catalogs/_data/',
-            _samples: '/dir/catalogs/_samples/',
+            catIndex: {
+                common: '/dir/catalogs/_designer/',
+                _designer: '/dir/catalogs/_designer/',
+                _data: '/dir/catalogs/_data/',
+                _samples: '/dir/catalogs/_samples/'    
+            },
             getResourceCatalogURL: function(theCatName, theResType, theResName){
-                var tmpBaseURL = '/';
+                var tmpBaseURL = './';
                 var tmpResType = ThisApp.controls.getUnifiedPluralName(theResType);
                 //--- Directory is tpl not templates
                 if( tmpResType == 'templates' ){
@@ -39,7 +41,13 @@ var ActionAppCore = {
                         var tmpAppName = ThisApp.common.designerResApp
                         tmpCatDir = '/' + tmpAppName + '/catalog/'
                     } else {
-                        tmpCatDir = './catalog/'
+                        tmpCatDir = ActionAppCore.util.getAppBaseURL() + 'catalog/'
+                    }
+                } else if( this.catIndex[theCatName]){
+                    if( ActionAppCore.util.isElectron() ){
+                        tmpCatDir = ActionAppCore.util.getAppBaseURL() + this.catIndex[theCatName] ;
+                    } else {
+                        tmpCatDir = this.catIndex[theCatName] ;
                     }
                 }
                 var tmpURL = tmpCatDir + tmpResType + '/' + theResName;
@@ -52,8 +60,8 @@ var ActionAppCore = {
         states: 'Alabama|AL,Alaska|AK,Arizona|AZ,Arkansas|AR,California|CA,Colorado|CO,Connecticut|CT,Delaware|DE,District Of Columbia|DC,Florida|FL,Georgia|GA,Hawaii|HI,Idaho|ID,Illinois|IL,Indiana|IN,Iowa|IA,Kansas|KS,Kentucky|KY,Louisiana|LA,Maine|ME,Maryland|MD,Massachusetts|MA,Michigan|MI,Minnesota|MN,Mississippi|MS,Missouri|MO,Montana|MT,Nebraska|NE,Nevada|NV,New Hampshire|NH,New Jersey|NJ,New Mexico|NM,New York|NY,North Carolina|NC,North Dakota|ND,Ohio|OH,Oklahoma|OK,Oregon|OR,Pennsylvania|PA,Rhode Island|RI,South Carolina|SC,South Dakota|SD,Tennessee|TN,Texas|TX,Utah|UT,Vermont|VT,Virginia|VA,Washington|WA,West Virginia|WV,Wisconsin|WI,Wyoming|WY',
         countries: 'Afghanistan|AF,Åland Islands|AX,Albania|AL,Algeria|DZ,American Samoa|AS,Andorra|AD,Angola|AO,Anguilla|AI,Antarctica|AQ,Antigua and Barbuda|AG,Argentina|AR,Armenia|AM,Aruba|AW,Australia|AU,Austria|AT,Azerbaijan|AZ,Bahamas|BS,Bahrain|BH,Bangladesh|BD,Barbados|BB,Belarus|BY,Belgium|BE,Belize|BZ,Benin|BJ,Bermuda|BM,Bhutan|BT,Bolivia, Plurinational State of|BO,Bonaire, Sint Eustatius and Saba|BQ,Bosnia and Herzegovina|BA,Botswana|BW,Bouvet Island|BV,Brazil|BR,British Indian Ocean Territory|IO,Brunei Darussalam|BN,Bulgaria|BG,Burkina Faso|BF,Burundi|BI,Cambodia|KH,Cameroon|CM,Canada|CA,Cape Verde|CV,Cayman Islands|KY,Central African Republic|CF,Chad|TD,Chile|CL,China|CN,Christmas Island|CX,Cocos (Keeling) Islands|CC,Colombia|CO,Comoros|KM,Congo|CG,Congo, the Democratic Republic of the|CD,Cook Islands|CK,Costa Rica|CR,Côte d\'Ivoire|CI,Croatia|HR,Cuba|CU,Curaçao|CW,Cyprus|CY,Czech Republic|CZ,Denmark|DK,Djibouti|DJ,Dominica|DM,Dominican Republic|DO,Ecuador|EC,Egypt|EG,El Salvador|SV,Equatorial Guinea|GQ,Eritrea|ER,Estonia|EE,Ethiopia|ET,Falkland Islands (Malvinas|FK,Faroe Islands|FO,Fiji|FJ,Finland|FI,France|FR,French Guiana|GF,French Polynesia|PF,French Southern Territories|TF,Gabon|GA,Gambia|GM,Georgia|GE,Germany|DE,Ghana|GH,Gibraltar|GI,Greece|GR,Greenland|GL,Grenada|GD,Guadeloupe|GP,Guam|GU,Guatemala|GT,Guernsey|GG,Guinea|GN,Guinea-Bissau|GW,Guyana|GY,Haiti|HT,Heard Island and McDonald Islands|HM,Holy See (Vatican City State|VA,Honduras|HN,Hong Kong|HK,Hungary|HU,Iceland|IS,India|IN,Indonesia|ID,Iran, Islamic Republic of|IR,Iraq|IQ,Ireland|IE,Isle of Man|IM,Israel|IL,Italy|IT,Jamaica|JM,Japan|JP,Jersey|JE,Jordan|JO,Kazakhstan|KZ,Kenya|KE,Kiribati|KI,Korea, Democratic People\'s Republic of|KP,Korea, Republic of|KR,Kuwait|KW,Kyrgyzstan|KG,Lao People\'s Democratic Republic|LA,Latvia|LV,Lebanon|LB,Lesotho|LS,Liberia|LR,Libya|LY,Liechtenstein|LI,Lithuania|LT,Luxembourg|LU,Macao|MO,Macedonia, the former Yugoslav Republic of|MK,Madagascar|MG,Malawi|MW,Malaysia|MY,Maldives|MV,Mali|ML,Malta|MT,Marshall Islands|MH,Martinique|MQ,Mauritania|MR,Mauritius|MU,Mayotte|YT,Mexico|MX,Micronesia, Federated States of|FM,Moldova, Republic of|MD,Monaco|MC,Mongolia|MN,Montenegro|ME,Montserrat|MS,Morocco|MA,Mozambique|MZ,Myanmar|MM,Namibia|NA,Nauru|NR,Nepal|NP,Netherlands|NL,New Caledonia|NC,New Zealand|NZ,Nicaragua|NI,Niger|NE,Nigeria|NG,Niue|NU,Norfolk Island|NF,Northern Mariana Islands|MP,Norway|NO,Oman|OM,Pakistan|PK,Palau|PW,Palestinian Territory, Occupied|PS,Panama|PA,Papua New Guinea|PG,Paraguay|PY,Peru|PE,Philippines|PH,Pitcairn|PN,Poland|PL,Portugal|PT,Puerto Rico|PR,Qatar|QA,Réunion|RE,Romania|RO,Russian Federation|RU,Rwanda|RW,Saint Barthélemy|BL,Saint Helena, Ascension and Tristan da Cunha|SH,Saint Kitts and Nevis|KN,Saint Lucia|LC,Saint Martin (French part|MF,Saint Pierre and Miquelon|PM,Saint Vincent and the Grenadines|VC,Samoa|WS,San Marino|SM,Sao Tome and Principe|ST,Saudi Arabia|SA,Senegal|SN,Serbia|RS,Seychelles|SC,Sierra Leone|SL,Singapore|SG,Sint Maarten (Dutch part|SX,Slovakia|SK,Slovenia|SI,Solomon Islands|SB,Somalia|SO,South Africa|ZA,South Georgia and the South Sandwich Islands|GS,South Sudan|SS,Spain|ES,Sri Lanka|LK,Sudan|SD,Suriname|SR,Svalbard and Jan Mayen|SJ,Swaziland|SZ,Sweden|SE,Switzerland|CH,Syrian Arab Republic|SY,Taiwan, Province of China|TW,Tajikistan|TJ,Tanzania, United Republic of|TZ,Thailand|TH,Timor-Leste|TL,Togo|TG,Tokelau|TK,Tonga|TO,Trinidad and Tobago|TT,Tunisia|TN,Turkey|TR,Turkmenistan|TM,Turks and Caicos Islands|TC,Tuvalu|TV,Uganda|UG,Ukraine|UA,United Arab Emirates|AE,United Kingdom|GB,United States|US,United States Minor Outlying Islands|UM,Uruguay|UY,Uzbekistan|UZ,Vanuatu|VU,Venezuela, Bolivarian Republic of|VE,Viet Nam|VN,Virgin Islands, British|VG,Virgin Islands, U.S|VI,Wallis and Futuna|WF,Western Sahara|EH,Yemen|YE,Zambia|ZM,Zimbabwe|ZW'
     },
-        /**
-     * getListSource
+    /**
+    * getListSource
     *  - Returns a source list use for dropdown and other like controls
     * 
     * Example: 
@@ -153,6 +161,26 @@ var ActionAppCore = {
         return tmpRet;
     },    
     util: {
+        isElectron(theUserAgent){
+            var tmpUserAgent = theUserAgent || navigator.userAgent;
+            if( tmpUserAgent ){
+                return (tmpUserAgent.toLowerCase().indexOf(' electron/') > -1)
+            }
+            return false;
+        },
+        getAppBaseURL(){
+            var tmpBase = window.location.pathname;
+            if( this.isElectron()){
+                tmpBase = tmpBase.replace('index.html','');
+            } else {
+                if( tmpBase == '' ){
+                    tmpBase = '.';
+                } if( tmpBase == '/' ){
+                    tmpBase = location.href;
+                }
+            }
+            return tmpBase;
+        },
         getStylesAsObject(theString){
             var regex = /([\w-]*)\s*:\s*([^;]*)/g;
             var match, properties={};
@@ -412,25 +440,42 @@ ActionAppCore.awaitInitialLoaders = function(){
 
 
 ActionAppCore.importTo = importTo;
-function importTo(theBaseURL, theModName, theTarget){
+function importTo(theBaseURL, theModNames, theTarget){
     var dfd = jQuery.Deferred();
-    // dfd.resolve(true);
-    // return dfd.promise();
 
-    if( !(theBaseURL && theModName) ){
-        dfd.reject("theBaseURL and theModName required");
-    } else {
+    var tmpGetImport = function(theBaseURL, theModName, theTarget){
         var tmpURL = theBaseURL + theModName + '.js';
         try {
-            var tmpTarget = theTarget || windows || {};
+            var tmpTarget = theTarget || window || {};
             import(tmpURL).then(function(theModule){
                 tmpTarget[theModName] = theModule[theModName];
-              dfd.resolve(tmpTarget);
+            dfd.resolve(tmpTarget);
             })
         } catch (error) {
             dfd.reject(error.toString())
             
         }
+    }
+    if( !(theBaseURL && theModNames) ){
+        dfd.reject("theBaseURL and theModName required");
+    } else {
+        var tmpModNames = theModNames;
+        if( typeof(tmpModNames) == 'string'){
+            tmpModNames = [tmpModNames];
+        }
+        var tmpToLoad = [];
+        for( var iPos in tmpModNames ){
+            var tmpModName = tmpModNames[iPos];
+            tmpToLoad.push(tmpGetImport(theBaseURL,tmpModName, theTarget))
+        }
+        if( tmpToLoad.length > 0){
+            $.whenAll(tmpToLoad).then(function(){
+                dfd.resolve(true);
+            })
+        } else {
+            dfd.resolve(true);
+        }
+
     }
     return dfd.promise();
   }
@@ -454,6 +499,7 @@ function importTo(theBaseURL, theModName, theTarget){
     addons: {}
   }
 
+  //--- ToDo: Move this to use generic method to import all and how to not have to adjust the url
   ActionAppCore.three.getThreeJSAddOn = getThreeJSAddOn;
   function getThreeJSAddOn(theOptions){
     if(!(theOptions && theOptions.type && theOptions.name)){
