e01b184d1a59764c9efcae93523ab9b48441190f
Joseph Francis <hookedupjoe@users.noreply.github.com>
2023-08-22 23:07:13 -0400

    auth2back

--- app.js
+++ app.js
@@ -628,7 +628,7 @@ if( $.designerConfig.isUsingData ){
     initAuth(app);
     initAuth2(app);
     initAuth(deployed);
-    initAuth2(deployed, true);
+    //initAuth2(deployed, true);
 }
 
 
@@ -1074,7 +1074,7 @@ function setup() {
             require('./preview-server/start').setup(deployed, deployedScope);
       
             
-            //initAuth2(deployed, true);
+            initAuth2(deployed, true);
 
             
             // error handlers
